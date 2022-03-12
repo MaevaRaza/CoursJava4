ed1ff1cff82d8ea733672100cd353f8921c56a64
maevarazakanaivo <maeva-fitia.razakanaivo@eleve.isep.fr>
2022-03-12 17:37:55 +0100

    TP4

---
+++ test/com/isep/rpg/HeroTest.java
@@ -0,0 +1,16 @@
+package com.isep.rpg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    public void testhjgu(){
+        assertEquals();
+    }
+
+    private void assertEquals() {
+    }
+}
