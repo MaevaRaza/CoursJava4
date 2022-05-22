73d09b3338dfac5ad8228157fbae1c041c8750e3
maevarazakanaivo <maeva-fitia.razakanaivo@eleve.isep.fr>
2022-05-22 15:10:25 +0200

    TP4

--- src/com/isep/rpg/BasicEnemy.java
+++ src/com/isep/rpg/BasicEnemy.java
@@ -4,7 +4,7 @@ public class BasicEnemy extends Enemy{
     public BasicEnemy(String role) {
         super(role,50, 0, 5);
     }
-/*test cc*/
+
     @Override
     public String toString() {
         return getRole() +  ":HP " + getLifePoints() + ",armure " + getArmor() + ", arme " + getWeaponDamage();
